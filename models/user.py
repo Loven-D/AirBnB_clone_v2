904d5c0d9ece683d54513a2bbe1521a9115d99ef
amyzi <ebiojoidris@icloud.com>
2023-11-21 13:31:45 +0000

    .

--- models/user.py
+++ models/user.py
@@ -1,5 +1,5 @@
-#!/usr/bin/python3
 """This module defines a class User"""
+
 import os
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
@@ -10,25 +10,23 @@ from models.base_model import BaseModel, Base
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = 'users'
+
     email = Column(
         String(128), nullable=False
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+    )
+
     password = Column(
         String(128), nullable=False
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+    )
+
     first_name = Column(
         String(128), nullable=True
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+    )
+
     last_name = Column(
         String(128), nullable=True
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
-    places = relationship(
-        'Place',
-        cascade="all, delete, delete-orphan",
-        backref='user'
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else None
-    reviews = relationship(
-        'Review',
-        cascade="all, delete, delete-orphan",
-        backref='user'
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else None
+    )
+
+    places = relationship('Place', backref='user', lazy='dynamic', cascade='delete')
+    reviews = relationship('Review', backref='user', lazy='dynamic', cascade='delete')
+
